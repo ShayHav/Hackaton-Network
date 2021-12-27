4b694be426dbfc3e40d18b3ff44f00226e1bdb73
Shahar-Dekstop <lankrys@post.bgu.ac.il>
2021-12-27 15:41:48 +0200

    server code

--- Client/Client.py
+++ Client/Client.py
@@ -1,6 +1,7 @@
 from socket import *
 import json
 
+
 class Client:
     udpPort = 13117
     magicCookie = 0xabcddcba
@@ -9,18 +10,21 @@ class Client:
     server_port = None
     tcp_socket = None
 
-    def __init__(self, team_name, ip_address=gethostname()):
+    def __init__(self, team_name):
         self.udpSocket = socket(AF_INET, SOCK_DGRAM)
-        self.udpSocket.bind((ip_address, self.udpPort))
+        self.udpSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        # TODO change after
+        self.ip = "192.168.1.210"
+        self.udpSocket.bind((self.ip, self.udpPort))
         self.team_name = team_name
 
     def receive_offer(self):
-        message, server_address = self.udpSocket.recvfrom(7)
+        message, server_address = self.udpSocket.recvfrom(1024)
         decoded_message = json.loads(message.decode())
         magic_cookie = decoded_message.get("magicCookie")
         message_type = decoded_message.get("messageType")
         server_port = decoded_message.get("tcpPort")
-        print(f"Received offer from {server_address},attempting to connect...")
+        print(f"Received offer from {server_address[0]},attempting to connect...")
         return magic_cookie, message_type, server_port, server_address
 
     def start_client(self):
@@ -29,13 +33,15 @@ class Client:
             magic_cookie, message_type, server_port, server_address = self.receive_offer()
             if magic_cookie == self.magicCookie and message_type == self.messageType:
                 try:
-                    tcp_socket = self.tcp_socket = socket(AF_INET, SOCK_STREAM)
-                    tcp_socket.connect((server_address, server_port))
+                    self.tcp_socket = socket(AF_INET, SOCK_STREAM)
+                    self.tcp_socket.connect((server_address[0], server_port))
                     message = f"{self.team_name}\n".encode("utf-8")
-                    tcp_socket.send(message)
+                    self.tcp_socket.send(message)
                     self.handle_game()
                 except InterruptedError:
                     print("got exception")
+                except ConnectionRefusedError:
+                    continue
                 finally:
                     self.tcp_socket.close()
                     print("Server disconnected, listening for offer requests...")
--- Server/Server.py
+++ Server/Server.py
@@ -4,10 +4,11 @@ import time
 import QuestionBank
 import json
 
+
 class Server:
     udpDstPort = 13117
-    server_udp_port = 12000
-    server_tcp_port = 12001
+    server_udp_port = 20001
+    server_tcp_port = 20000
     magicCookie = 0xabcddcba
     messageType = 0x2
     message = None
@@ -16,45 +17,34 @@ class Server:
     c_in = threading.Condition()
     outgoing_messages = []
     c_out = threading.Condition()
+    send_offers = True
 
     def __init__(self):
         self.question_bank = QuestionBank.QuestionBank()
         self.server_udp_socket = socket(AF_INET, SOCK_DGRAM)
+        self.server_udp_socket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
         self.welcoming_tcp_socket = socket(AF_INET, SOCK_STREAM)
-        self.welcoming_tcp_socket.setblocking(False)
         # TODO change after
-        self.ip = "10.0.0.38"
+        self.ip = "192.168.1.210"
         self.server_udp_socket.bind((self.ip, self.server_udp_port))
-        self.welcoming_tcp_socket.bind((gethostname(), self.server_tcp_port))
-        self.welcoming_tcp_socket.listen(2)
+        self.welcoming_tcp_socket.bind((self.ip, self.server_tcp_port))
+        self.welcoming_tcp_socket.listen()
 
-    def send_offers(self):
-        number_of_players = 0
-        conn1, client_address1 = None, None
-        conn2, client_address2 = None, None
-
+    def manage_server(self):
         print(f"Server started, listening on IP address {self.ip}")
-        message = json.dumps({"magicCookie": self.magicCookie,
-                              "messageType":  self.messageType,
-                              "tcpPort": self.server_tcp_port})
+
+        offer_thread = threading.Thread(target=self.send_udp_offers, args=[])
+        offer_thread.start()
+
         # accepting two player
-        while number_of_players < 2:
-            self.server_udp_socket.sendto(message.encode(), ("255.255.255.255", self.udpDstPort))
-            time.sleep(1)
-
-            if not conn1 and not client_address1:
-                conn1, client1_address = self.welcoming_tcp_socket.accept()
-                if conn1:
-                    number_of_players = number_of_players + 1
-
-            if not conn2 and not client_address2:
-                conn2, client2_address = self.welcoming_tcp_socket.accept()
-                if conn2:
-                    number_of_players = number_of_players + 1
+        conn1, client1_address = self.welcoming_tcp_socket.accept()
+        conn2, client2_address = self.welcoming_tcp_socket.accept()
+        print(client1_address, client2_address)
+        self.send_offers = False
 
         # creating the Threads, one to each player and starting the game
-        player1 = threading.Thread(target=self.receive_player, args=(conn1, client_address1, 0))
-        player2 = threading.Thread(target=self.receive_player, args=(conn2, client_address2, 1))
+        player1 = threading.Thread(target=self.receive_player, args=[conn1, client1_address, 0])
+        player2 = threading.Thread(target=self.receive_player, args=[conn2, client2_address, 1])
 
         player1.start()
         player2.start()
@@ -66,6 +56,14 @@ class Server:
 
         # clean server resources after the game
         self.clean()
+
+    def send_udp_offers(self):
+        message = json.dumps({"magicCookie": self.magicCookie,
+                              "messageType":  self.messageType,
+                              "tcpPort": self.server_tcp_port})
+        while self.send_offers:
+            self.server_udp_socket.sendto(message.encode(), ("255.255.255.255", self.udpDstPort))
+            time.sleep(1)
 
     def clean(self):
         self.message = None
@@ -83,7 +81,6 @@ class Server:
                 return
             self.c_in.acquire()
             if len(self.answers) == 0:
-                self.c_in.release()
                 self.c_in.wait(10)
             else:
                 break
@@ -100,23 +97,22 @@ class Server:
         self.outgoing_messages.append(f"""Game over!
                              The correct answer was {correct_answer}
                              Congratulations to the winner: {self.teams_names[winner]}""")
-        self.c_out.release()
         self.c_out.notify_all()
+        self.c_out.release()
 
     def draw(self):
         self.c_out.acquire()
         self.outgoing_messages.append(f"""Game over!
                                      The correct answer was {self.question_bank.get_answer()}
                                      The game was ended with a draw""")
-        self.c_out.release()
         self.c_out.notify_all()
+        self.c_out.release()
 
     def assemble_message(self):
         can_assemble = False
         while not can_assemble:
             self.c_in.acquire()
             if not (self.teams_names[0] and self.teams_names[1]):
-                self.c_in.release()
                 self.c_in.wait()
             else:
                 can_assemble = True
@@ -128,28 +124,29 @@ class Server:
                       ==
                       Please answer the following question as fast as you can:
                       {question}"""
-        self.c_in.release()
+        self.c_out.acquire()
         self.c_out.notify_all()
+        self.c_out.release()
+        self.c_in.release()
 
     def receive_player(self, conn, address, player_id):
         # get the team name of the player and save it
-        team_name = conn.recvfrom(1024).decode("utf-8")
+        team_name = conn.recv(1024).decode("utf-8")
         self.c_in.acquire()
         self.teams_names.insert(player_id, team_name)
-        self.c_in.release()
         self.c_in.notify_all()
+        self.c_in.release()
         # sending the message
         self.send_question(conn, address)
         # getting the answer from the player
-        answer = conn.recvfrom(1024).decode("utf-8")
+        answer = conn.recv(1024).decode("utf-8")
         self.c_in.acquire()
         self.answers.append((player_id, answer))
-        self.c_in.release()
         self.c_in.notify_all()
+        self.c_in.release()
         # sending the response to the player
         self.c_in.acquire()
         if len(self.outgoing_messages) == 0:
-            self.c_in.release()
             self.c_in.wait()
         self.c_in.acquire()
         message = self.outgoing_messages[0].encode("utf-8")
@@ -163,7 +160,6 @@ class Server:
             message_to_send = self.message.encode("utf-8")
             conn.sendto(message_to_send, address)
         else:
-            self.c_out.release()
             self.c_out.wait()
             self.c_out.acquire()
             message_to_send = self.message.encode("utf-8")
@@ -173,7 +169,7 @@ class Server:
     def start(self):
         while True:
             try:
-                self.send_offers()
+                self.manage_server()
             except InterruptedError:
                 print("got message")
 
